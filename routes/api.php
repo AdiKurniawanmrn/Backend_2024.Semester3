7be487be5b191d353ceeb44da634508d1f07c6b9
AdiKurniawanmrn <146794703+AdiKurniawanmrn@users.noreply.github.com>
2024-11-13 17:49:28 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,12 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/pegawai', [PegawaiController::class,'index']);
-Route::post('/pegawai', [PegawaiController::class,'store']);
+use App\Http\Controllers\PegawaiController;
+
+
+Route::get('/pegawais', [PegawaiController::class, 'index']);
+Route::post('/pegawais', [PegawaiController::class, 'store']);
+Route::get('/pegawais/{id}', [PegawaiController::class, 'show']);
+Route::put('/pegawais/{id}', [PegawaiController::class, 'update']);
+Route::delete('/pegawais/{id}', [PegawaiController::class, 'destroy']);
+
