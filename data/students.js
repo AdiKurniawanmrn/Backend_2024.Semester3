fe3c1388db7727c4122c7f6ee2d6eff2f4ff782a
AdiKurniawanmrn <146794703+AdiKurniawanmrn@users.noreply.github.com>
2025-01-09 01:58:49 +0700

    students.js

--- data/students.js
+++ data/students.js
@@ -1,10 +1,10 @@
 // TODO 1: Buat data students
-const students = [
+const Student = [
     { id: 1, name: "Mikel" },
     { id: 2, name: "Hannah" },
     { id: 3, name: "Jonas" }
   ];
   
-  // TODO 2: Export data students
-  export default students;
+  // TODO 2: Export data Student
+  module.exports = Student;
   
